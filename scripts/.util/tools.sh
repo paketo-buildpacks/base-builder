8672912f43b748b0cf925aa87e2d7380318f31f1
Sophie Wigmore <swigmore@vmware.com>
2020-12-17 15:44:40 -0500

    Cosmetic update to scripts (#57)
    Signed-off-by: Sophie Wigmore <swigmore@vmware.com>
    
    Co-authored-by: Forest Eckhardt <feckhardt@pivotal.io>

--- scripts/.util/tools.sh
+++ scripts/.util/tools.sh
@@ -50,11 +50,11 @@ function util::tools::pack::install() {
 
   if [[ ! -f "${dir}/pack" ]]; then
     local version
-    if [[ ! -f "${BUILDPACKDIR}/.github/pack-version" ]]; then
+    if [[ ! -f "${BUILDERDIR}/.github/pack-version" ]]; then
       util::print::error "Pack version file (.github/pack-version) not found"
     fi
 
-    version="v$(cat "${BUILDPACKDIR}"/.github/pack-version)"
+    version="v$(cat "${BUILDERDIR}"/.github/pack-version)"
 
     util::print::title "Installing pack ${version}"
     curl "https://github.com/buildpacks/pack/releases/download/${version}/pack-${version}-${os}.tgz" \
